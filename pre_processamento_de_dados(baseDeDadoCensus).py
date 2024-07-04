c8da6f5774f487aef786428b709ca849c3f057d0
João Vitor <joaovitorferreiragoncalves3691@gmail.com>
2024-07-04 14:43:17 -0300

    Dia2Aprendendo

--- pre_processamento_de_dados(baseDeDadoCensus).py
+++ pre_processamento_de_dados(baseDeDadoCensus).py
@@ -3,9 +3,46 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
 
 base_census = pd.read_csv('census.csv')
 print(base_census)
 print(base_census.isnull().sum())
 print(base_census.describe())
+
+#grafico = px.treemap(base_census, path=['workclass', 'age'])
+#grafico.show()
+
+x_census = base_census.iloc[:, 0:14].values
+y_census = base_census.iloc[:, 14].values
+
+label_encoder_teste = LabelEncoder()
+
+#teste = label_encoder_teste.fit_transform(x_census[:, 1])
+
+label_encoder_workclass = LabelEncoder()
+label_encoder_education = LabelEncoder()
+label_encoder_marital = LabelEncoder()
+label_encoder_occupation = LabelEncoder()
+label_encoder_relationship = LabelEncoder()
+label_encoder_race = LabelEncoder()
+label_encoder_sex = LabelEncoder()
+label_encoder_country = LabelEncoder()
+
+x_census[:, 1] = label_encoder_workclass.fit_transform(x_census[:, 1])
+x_census[:, 3] = label_encoder_education.fit_transform(x_census[:, 3])
+x_census[:, 5] = label_encoder_marital.fit_transform(x_census[:, 5])
+x_census[:, 6] = label_encoder_occupation.fit_transform(x_census[:, 6])
+x_census[:, 7] = label_encoder_relationship.fit_transform(x_census[:, 7])
+x_census[:, 8] = label_encoder_race.fit_transform(x_census[:, 8])
+x_census[:, 9] = label_encoder_sex.fit_transform(x_census[:, 9])
+x_census[:, 13] = label_encoder_country.fit_transform(x_census[:, 13])
+
+print(x_census)
+
+oneHotEncoder_census = ColumnTransformer(transformers=[('OneHot', OneHotEncoder(), [1,3,5,6,7,8,9,13])], remainder='passthrough')
+x_census = oneHotEncoder_census.fit_transform(x_census).toarray()
+
+print(x_census[0])
